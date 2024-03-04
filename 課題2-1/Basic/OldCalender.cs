0ddf6ec9905c6635064792059815c942d7ab0e4f
Yabiee-5 <e205744@ie.u-ryukyu.ac.jp>
2024-03-04 16:22:13 +0900

    課題2-1,課題2-2-2も提出用に追加

---
+++ 課題2-1/Basic/OldCalender.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Basic
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class OldCalender
+    {
+        private JapaneseLunisolarCalendar jlCalender = new JapaneseLunisolarCalendar();
+
+        public enum Rokuyos
+        {
+            大安,
+            赤口,
+            先勝,
+            友引,
+            先負,
+            仏滅
+        }
+
+        public OldCalender()
+        {
+        }
+
+        public DateTime ConvOldCalender(DateTime newDateTime)
+        {
+            // int lunarEra = jlCalender.GetEra(newDateTime);
+            int lunarYear = jlCalender.GetYear(newDateTime);
+            int lunarMonth = jlCalender.GetMonth(newDateTime);
+            int lunarDay = jlCalender.GetDayOfMonth(newDateTime);
+
+            // int leapMon = jlCalender.GetLeapMonth(lunarYear, lunarEra);
+            switch (lunarMonth)
+            {
+                case 13:// 閏月の場合は調整.
+                    lunarMonth -= 1;
+                    DateTime leapYearDate = new DateTime(lunarYear, lunarMonth, lunarDay);
+                    return leapYearDate;
+
+                default:
+                    DateTime lunDate = new DateTime(lunarYear, lunarMonth, lunarDay);
+                    return lunDate;
+            }
+        }
+
+
+        public void PrintRokuyo(int mon, int day)
+        {
+            int key = (mon + day) % 6;
+
+
+            switch (key)
+            {
+                case 0:
+                    Println(Rokuyos.大安);
+                    return;
+                case 1:
+                    Println(Rokuyos.赤口);
+                    return;
+                case 2:
+                    Println(Rokuyos.先勝);
+                    return;
+                case 3:
+                    Println(Rokuyos.友引);
+                    return;
+                case 4:
+                    Println(Rokuyos.先負);
+                    return;
+                default:
+                    Println(Rokuyos.仏滅);
+                    return;
+            }
+
+
+        }
+
+        public void Println(Rokuyos rokuyo) {
+            Console.WriteLine("{0}", rokuyo);
+        }
+    }
+}
---
+++ 課題2-2-2/Technical/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Technical
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // 商品を追加
+            Yaoya u1 = new Yaoya();
+            IStaff staff = (IStaff)u1;
+            staff.AddProduct(new Vegetable("きゃべつ", 200));
+            staff.AddProduct(new Vegetable("にんじん", 100));
+            staff.AddProduct(new Vegetable("じゃがいも", 150));
+            staff.AddProduct(new Vegetable("とまと", 300));
+            staff.AddProduct(new Vegetable("だいこん", 130));
+
+            // 商品を購入
+            IPayment customer = (IPayment)u1;
+
+            while (true)
+            {
+                customer.PrintProduct();
+                int id = 0;
+                try
+                {
+                    id = int.Parse(Console.ReadLine());
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("数値を入力してください.");
+                }
+
+                // 合計と表示
+                customer.CalcTotalPrice(id);
+                customer.PrintReceipt();
+
+                Console.WriteLine("続けて購入しますか？ 続ける:Y、続けない:N\n");
+                string userInput = Console.ReadLine();
+                if (userInput == "Y")
+                {
+                    continue;
+                }
+                else if (userInput == "N")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("{0}は無効な入力です.購入を終了します.", userInput);
+                    break;
+                }
+
+            }
+
+        }
+
+        
+    }
+}
