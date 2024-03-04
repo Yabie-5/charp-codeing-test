891f511b747d371ae07fa66b9e62e85d0b4a31d9
Yabiee-5 <e205744@ie.u-ryukyu.ac.jp>
2024-03-04 15:56:47 +0900

    課題2-2-1をコミット

---
+++ 課題2-2-1/Technical/Vegetable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Technical
+{
+
+    public class Vegetable
+    {
+        public string VegetableName { set; get; }
+
+        public int Price { set; get; }
+
+        public Vegetable(string vegetableName, int price)
+        {
+            VegetableName = vegetableName;
+            Price = price;
+        }
+
+    }
+}
---
+++ 課題2-2-1/Technical/Yaoya.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Technical
+{
+    using System.Collections.Generic;
+
+    public class Yaoya : IPayment, IStaff
+    {
+        private List<Vegetable> products = new List<Vegetable>();
+        public int totalPrice
+        {
+            private set; get;
+        }
+        public Yaoya()
+        {
+            totalPrice = 0;
+        }
+
+        public void AddProduct(Vegetable vegetable)
+        {
+            products.Add(vegetable);
+        }
+
+        public void PrintProduct()
+        {
+            if (products.Count > 0)
+            {
+                Console.WriteLine("どの野菜を購入しますか?");
+                for (int i = 0; i<products.Count; i++)
+                {
+                    Console.WriteLine("{0}:{1} ({2}円)",i+1,products[i].VegetableName, products[i].Price);
+                }
+
+            }
+            else {
+                Console.WriteLine("商品がありません\n");
+            }
+
+        }
+
+
+        public int CalcTotalPrice(int id) {
+            // コンソールの表示が1~なので,Listに会うようにindex調整
+            id -= 1;
+            try
+            {
+                totalPrice += products[id].Price;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("範囲外の入力です. 1~{0}で入力してください.",products.Count+1);
+            }
+            return totalPrice;
+        }
+
+        public void PrintReceipt()
+        {
+            Console.WriteLine("合計: {0}円\n",totalPrice);
+        }
+
+        
+    }
+}
